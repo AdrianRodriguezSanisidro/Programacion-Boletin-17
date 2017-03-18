eddce9fc419b9f9622ce993a7e2f3f67433c0af2
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-03-18 22:00:45 +0100

    Nuevo array con los nombres de los alumnos y metodo par visualizar la nota de un alumno

--- Boletin17_2y3/src/boletin17_2y3/Clase.java
+++ Boletin17_2y3/src/boletin17_2y3/Clase.java
@@ -1,11 +1,14 @@
 package boletin17_2y3;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Adry
  */
 public class Clase {
-   int[]notas={1,6,7,9,7,5,6,9,10,8,3,6,8,5,6,7,9,5,4,8,9,7,4,5,6,6,1,3,9,8};
+   int[]notas={1,6,7,9,7,5,6,9,10,8,3,6,8,9,10,7,9,5,4,8,9,7,4,5,6,6,1,3,9,8};
+   String[]alumnos={"Pepe","Juan","Ana","Luis","Carlos","Carla","Roberto","Richard","Cristina","Luisa","Clara","Ricardo","Eloisa","Sandra","Adrian","Tamara","Ainhoa","Diego","Maria","Gena","Alba","Laura","Iago","Isaac","Marco","Aaron","Lucia","Aida","David","Jesus"};
    int aprobados,suspensos=0;
    public void visualizarNotas(){
     for(int i=0;i<notas.length;i++){
@@ -30,4 +33,15 @@ public class Clase {
     }
        System.out.println("La nota mas alta es "+mejorNota);
 }
+   public void visualizarNotaAlumno(){
+       boolean encontrado=false;
+       String aux=JOptionPane.showInputDialog("Nombre del alumno:");
+       for(int i=0;i<alumnos.length;i++){
+           if(aux.equals(alumnos[i]))
+               System.out.println("A nota de "+alumnos[i]+" e "+notas[i]);
+               encontrado=true;
+       }
+       if(encontrado==false)
+           System.out.println("O alumno non existe");
+   }
 }
