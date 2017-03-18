ee1a290efd97884dd66cd5cb5472bbd4805eb8c0
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-03-18 21:21:55 +0100

    Solucionado un alumno de mas y mostrar por pantalla

--- Boletin17_2y3/src/boletin17_2y3/Clase.java
+++ Boletin17_2y3/src/boletin17_2y3/Clase.java
@@ -5,13 +5,14 @@ package boletin17_2y3;
  * @author Adry
  */
 public class Clase {
-   int[]notas={1,6,7,9,7,5,6,9,10,8,3,6,2,8,5,6,7,9,5,4,8,9,7,4,5,6,6,1,3,9,8};
-   int aprobados,suspensos;
+   int[]notas={1,6,7,9,7,5,6,9,10,8,3,6,8,5,6,7,9,5,4,8,9,7,4,5,6,6,1,3,9,8};
+   int aprobados,suspensos=0;
    public void visualizarNotas(){
     for(int i=0;i<notas.length;i++){
         if(notas[i]>=5)
             aprobados++;
         else suspensos++;
     }
+       System.out.println("Aprobaron "+aprobados+" y suspendieron "+suspensos);
 }
 }
