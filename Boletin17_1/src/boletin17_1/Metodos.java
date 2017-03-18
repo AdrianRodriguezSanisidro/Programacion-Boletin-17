9b0546b855c233f18559ba929c045b7ead02de5c
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-03-18 20:59:29 +0100

    Metodos hechos

--- Boletin17_1/src/boletin17_1/Metodos.java
+++ Boletin17_1/src/boletin17_1/Metodos.java
@@ -6,9 +6,17 @@ package boletin17_1;
  */
 public class Metodos {
     int[]numeros=new int[6];
+    /*
+    Hacer constructores
+    */
     public void cargarArray(){
         for(int i=0;i<numeros.length;i++){
-            numeros[i]=(int)Math.random();
+            numeros[i]=(int)(Math.random()*49+1);
         }
+    }
+    public void visualizarArray(){
+        System.out.println("Numeros: ");
+        for(int i=5;i>=0;i--)
+            System.out.print(numeros[i]+" ");
     }
 }
