7782be5b17b4289e0a306ded20f2708a9a184d14
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-03-18 22:15:29 +0100

    Llamo a los metodos

--- Boletin17_2y3/src/boletin17_2y3/Boletin17_2y3.java
+++ Boletin17_2y3/src/boletin17_2y3/Boletin17_2y3.java
@@ -10,7 +10,13 @@ public class Boletin17_2y3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+       Clase obj1=new Clase();
+//       obj1.visualizarNotas();
+//       obj1.calcularMedia();
+//       obj1.notaAlta();
+//       obj1.visualizarNotaAlumno();
+//       obj1.alumnosAprobados();
+         obj1.ordenarPorNota();
     }
     
 }
